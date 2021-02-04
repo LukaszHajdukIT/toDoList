ef6f43c13f0ed778dfbeed84867e35091b6d8e2d
One Visage <onevisage@interia.pl>
2021-02-04 10:16:40 +0100

    added function which adds new tasks

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let $toDoInput; // place where user input text task
 let $alertInfo; // info about no tasks / need for add text
 let $addBtn; // button ADD - add new elements to list
 let $ulList; // ours task list, tags <ul></ul>
+let $newTask; // new added li, new task
 
 const main = () => {
     prepareDOMElements();
@@ -10,18 +11,28 @@ const main = () => {
 
 // get ours elements
 const prepareDOMElements = () => {
-    $toDoInput = document.querySelector('.toDoInput');
+    $toDoInput = document.querySelector('.todoInput');
     $alertInfo = document.querySelector('.alertInfo');
     $addBtn = document.querySelector('.addBtn');
-    $ulList = document.querySelector('.toDoList ul');
+    $ulList = document.querySelector('.todoList ul');
 };
 
 // broadcasting listener
-const prepareDOMEvents = () => {};
+const prepareDOMEvents = () => {
+    $addBtn.addEventListener('click', addNewTask);
+};
 
+const addNewTask = () => {
+    if ($toDoInput.value !== '') {
+        $newTask = document.createElement('li');
+        $newTask.innerText = $toDoInput.value;
+        $ulList.appendChild($newTask);
 
-
-
-
+        $toDoInput.value = '';
+        $alertInfo.innerText = '';
+    } else {
+        $alertInfo.innerText = 'Wpisz treść zadania!';
+    }
+}
 
 document.addEventListener('DOMContentLoaded', main);
