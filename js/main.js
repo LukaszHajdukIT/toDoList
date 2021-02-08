e945422c23f85e4db42373202d5421d5a7e1f38d
One Visage <onevisage@interia.pl>
2021-02-08 19:24:46 +0100

    tools area added

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let $alertInfo; // info about no tasks / need for add text
 let $addBtn; // button ADD - add new elements to list
 let $ulList; // ours task list, tags <ul></ul>
 let $newTask; // new added li, new task
+let toolsPanel;
 
 const main = () => {
     prepareDOMElements();
@@ -20,6 +21,7 @@ const prepareDOMElements = () => {
 // broadcasting listener
 const prepareDOMEvents = () => {
     $addBtn.addEventListener('click', addNewTask);
+    $ulList.addEventListener('click', checkClick);
 };
 
 const addNewTask = () => {
@@ -30,9 +32,37 @@ const addNewTask = () => {
 
         $toDoInput.value = '';
         $alertInfo.innerText = '';
+        cerateToolsArea();
     } else {
         $alertInfo.innerText = 'Wpisz treść zadania!';
     }
 }
+
+const cerateToolsArea = () => {
+    toolsPanel = document.createElement('div');
+    toolsPanel.classList.add('tools')
+    $newTask.appendChild(toolsPanel);
+
+    let completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+    toolsPanel.appendChild(completeBtn);
+
+    let editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.innerText = 'EDIT';
+    toolsPanel.appendChild(editBtn);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+    toolsPanel.appendChild(deleteBtn);
+}
+
+const checkClick = (event) => {
+    if(event.target.closest('button').classList.contains('complete')){
+        console.log(event.target);
+    }
+};
 
 document.addEventListener('DOMContentLoaded', main);
