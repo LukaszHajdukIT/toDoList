f9886b0635d386b12c664f328e91051df838d863
One Visage <onevisage@interia.pl>
2021-02-12 14:03:37 +0100

    tools done

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ let $alertInfo; // info about no tasks / need for add text
 let $addBtn; // button ADD - add new elements to list
 let $ulList; // ours task list, tags <ul></ul>
 let $newTask; // new added li, new task
-let toolsPanel;
+let toolsPanel; // panel with check, edit and delete buttons
+
+let $popup; // popup getter
+let $popupInfo; // alert in popup, when add empty string
+let $editedTodo; // edited ToDo
+let $popupInput; // tekst added to input in popup
+let $addPopupBtn; // button "accept" in popup
+let $closeTodoBtn; // button to exit popup
 
 const main = () => {
     prepareDOMElements();
@@ -16,12 +23,18 @@ const prepareDOMElements = () => {
     $alertInfo = document.querySelector('.alertInfo');
     $addBtn = document.querySelector('.addBtn');
     $ulList = document.querySelector('.todoList ul');
+    $popup = document.querySelector('.popup');
+    $popupInfo = document.querySelector('.popupInfo');
+    $popupInput = document.querySelector('.popupInput');
+    $addPopupBtn = document.querySelector('.accept');
+    $closeTodoBtn = document.querySelector('.cancel');
 };
 
 // broadcasting listener
 const prepareDOMEvents = () => {
     $addBtn.addEventListener('click', addNewTask);
     $ulList.addEventListener('click', checkClick);
+    $closeTodoBtn.addEventListener('click', closePopup);
 };
 
 const addNewTask = () => {
@@ -34,7 +47,7 @@ const addNewTask = () => {
         $alertInfo.innerText = '';
         cerateToolsArea();
     } else {
-        $alertInfo.innerText = 'Wpisz treść zadania!';
+        $alertInfo.innerText = 'Wpisz treść zadanie!';
     }
 }
 
@@ -60,9 +73,22 @@ const cerateToolsArea = () => {
 }
 
 const checkClick = (event) => {
-    if(event.target.closest('button').classList.contains('complete')){
-        console.log(event.target);
+    if (event.target.closest('button').classList.contains('complete')) {
+        event.target.closest('li').classList.toggle('completed');
+        event.target.closest('button').classList.toggle('completed');
+    } else if (event.target.closest('button').className === 'edit') {
+        editTask();
+    } else if (event.target.closest('button').className === 'delete') {
+        console.log('delete');
     }
 };
+
+const editTask = () => {
+    $popup.style.display = 'flex';
+};
+
+const closePopup = () => {
+    $popup.style.display = 'none';
+}
 
 document.addEventListener('DOMContentLoaded', main);
